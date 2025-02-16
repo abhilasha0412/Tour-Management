2cce500f5a12b380a07ead574208a154cc23c5fb
abhilasha0412 sahu <abhilashasahu0412@gmail.com>
2025-02-16 19:50:55 +0000

    frontend

--- tour-management/frontend/src/components/Image-gallery/MasonryImagesGallery.jsx
+++ tour-management/frontend/src/components/Image-gallery/MasonryImagesGallery.jsx
@@ -1,20 +1,25 @@
-import galleryImg01 from "../../assets/images/gallery-01.jpg"
-import galleryImg02 from "../../assets/images/gallery-02.jpg"
-import galleryImg03 from "../../assets/images/gallery-03.jpg"
-import galleryImg04 from "../../assets/images/gallery-04.jpg"
-import galleryImg05 from "../../assets/images/gallery-05.jpg"
-import galleryImg06 from "../../assets/images/gallery-06.jpg"
-import galleryImg07 from "../../assets/images/gallery-07.jpg"
+import React from "react";
+import galleryImages from "./galleryImages";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
-const galleryImages = [
-    galleryImg01,
-    galleryImg02,
-    galleryImg03,
-    galleryImg04,
-    galleryImg05,
-    galleryImg06,
-    galleryImg07,
-];
+const MasonryImagesGallery = () => {
+    return (
+        <ResponsiveMasonry columnsCountBreakPoints={{350:1, 768:3, 992:4}}>
+            <Masonry gutter="1rem">
+                 {
+                    galleryImages.map((items,index)=>(
+                        <img 
+                        className="masonry__img"
+                        src={items} 
+                        key={index} 
+                        alt="" 
+                        style={{width:"100%", display: "block", borderRadius: "10px"}} 
+                        />
+                    ))}
+            </Masonry>
 
+        </ResponsiveMasonry>
+    );
+};
 
-export default galleryImages;
+export default MasonryImagesGallery;
