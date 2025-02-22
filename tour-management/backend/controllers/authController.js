a38d6cab18523708fbb495dd322e4760429e0337
abhilasha0412 sahu <abhilashasahu0412@gmail.com>
2025-02-22 08:30:46 +0000

    backend

--- tour-management/backend/controllers/authController.js
+++ tour-management/backend/controllers/authController.js
@@ -61,4 +61,4 @@ export const login = async(req,res) =>{
 
     }
 };
-    
+
--- tour-management/backend/routes/users.js
+++ tour-management/backend/routes/users.js
@@ -2,6 +2,9 @@ import express from "express";
 import { deleteUser, getAllUser, getSingleUser, updateUser } from "../controllers/userController.js";
 const router = express.Router();
 
+
+import { verifyUser } from "../utils/verifyToken.js";
+
 // update user 
 router.put("/:id", updateUser);
 
@@ -9,7 +12,7 @@ router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
 // get single user 
-router.get("/:id", getSingleUser);
+router.get("/:id", verifyUser ,getSingleUser);
 
 // get all user
 router.get("/", getAllUser);
