77424fad2473d4e08a0a05b9271e7913c0c7207d
abhilasha0412 sahu <abhilashasahu0412@gmail.com>
2025-03-16 15:07:00 +0000

    frontend

--- tour-management/frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ tour-management/frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -1,13 +1,25 @@
 import React from "react"; //6.9k (gzipped: 2.7k)
 import TourCard from "../../shared/TourCard";
-import tourData from "../../assets/data/tours";
+
 import { Col } from "reactstrap";
+import useFetch from "../../hooks/useFetch";
+import { BASE_URL } from "../../utils/config";
+
 
 const FeaturedTourList = () => {
+
+    const {data: featuredTours, loading, error} = useFetch(`${BASE_URL}/tours/search/getFeaturedTours`);
+    console.log(featuredTours);
     return ( <> 
     {
-        tourData?.map(tour=>(
-            <Col lg="3" className="mb-4" key={tour.id}>
+        loading && <h4> Loading..........</h4>
+    }
+    {
+        error && <h4>{error}</h4>
+    }
+    {
+       !loading && !error && featuredTours?.map(tour=>(
+            <Col lg="3" md="6" sm="6" className="mb-4" key={tour.id}>
                 <TourCard tour={tour} />
             </Col>
         ))}
---
+++ tour-management/frontend/src/utils/config.js
@@ -0,0 +1,2 @@
+
+export const BASE_URL ='https://5000-abhilasha04-tourmanagem-rbhjbr6r19c.ws-us117.gitpod.io'
